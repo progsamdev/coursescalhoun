86fa3778a32a1a63f0bc0a00f202929867ca50c3
samuel silva <prog.samdev@gmail.com>
2024-01-06 14:53:06 -0300

    Section 1: some comments

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> Welcome to my awesome site! </h1>") //this is the response from the server
+	fmt.Fprint(w, "<h1> Welcome to my awesome site! </h1>")
+	//w is responsable for the response
+	//r is the request
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", handlerFunc) // "/" path, handlerFunc is the func that will process the web request
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", nil) //build server in port 3000
 }
