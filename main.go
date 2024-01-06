705c749f1ce655fb2012a386963bcd8cb0ad8162
samuel silva <prog.samdev@gmail.com>
2024-01-06 16:35:53 -0300

    Section 2: Custom Routing

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+const PAGE_NOT_FOUND = "PAGE NOT FOUND"
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8") //specify the type of content so browser can handle it properly // set instead of add so we override any other setting
 	fmt.Fprint(w, "<h1> Welcome to my cool site! </h1>")
@@ -13,8 +15,32 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	//r is the request
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1> Contact page </h1> <p> To get in touch, email me at <a href=\"mailto:samuel.msbr@gmail.com\">samuel.msbr@gmail.com</a>.")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		msg := PAGE_NOT_FOUND
+		errorHandler(w, r, msg, http.StatusNotFound)
+	}
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, msg string, status int) {
+	w.WriteHeader(status)
+	fmt.Fprintf(w, "%s : %v", msg, status)
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc) // "/" path, handlerFunc is the func that will process the web request
+	http.HandleFunc("/", pathHandler) // "/" path, homeHandler is the func that will process the web request
+	//	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil) //build server in port 3000
 }
