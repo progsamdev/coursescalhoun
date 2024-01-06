d419918491dcb7def192fc25fbc7ec66b5470ad6
samuel silva <prog.samdev@gmail.com>
2024-01-06 15:42:37 -0300

    Section 2: add gitignore

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> Welcome to my awesome site! </h1>")
+	fmt.Fprint(w, "<h1> Welcome to my cool site! </h1>")
 	//w is responsable for the response
 	//r is the request
 }
