0d082d830c2396f7d1fd9950a887c1a27ba83c8c
samuel silva <prog.samdev@gmail.com>
2024-01-08 23:19:25 -0300

    Section 2: Exploring Handler Conversions

--- main.go
+++ main.go
@@ -5,6 +5,20 @@ import (
 	"net/http"
 )
 
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	}
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8") //specify the type of content so browser can handle it properly // set instead of add so we override any other setting
@@ -30,23 +44,14 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 // 	}
 // }
 
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-	}
-}
-
 func main() {
 	//http.HandleFunc("/", pathHandler) // "/" path, homeHandler is the func that will process the web request
 	//	http.HandleFunc("/contact", contactHandler)
+
+	// http.Handler -> interface with the ServeHTTP method
+	// http.Handle("/", http.Handler) => handle receives a Handler
+	// http.HandlerFunc -> a function type that accepts same args as ServeHTTP method. also implements http.Handler
+	// http.HandleFunc("/contact", contactHandler) -> receives a handlerFunc
 	var router Router
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", router)
