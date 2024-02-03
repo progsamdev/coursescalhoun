61a2417fe2407e62db023a18282b343c595d77ce
samuel silva <prog.samdev@gmail.com>
2024-02-03 16:11:11 -0300

    Created interface for Template

--- controllers/users.go
+++ controllers/users.go
@@ -2,13 +2,11 @@ package controllers
 
 import (
 	"net/http"
-
-	"github.com/progsamdev/coursescalhoun/views"
 )
 
 type Users struct {
 	Templates struct {
-		New *views.Template
+		New Template
 	}
 }
 
