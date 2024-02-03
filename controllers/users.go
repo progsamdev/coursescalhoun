269ee0b1c3db2a200fbe90e3d88ce25305398df8
samuel silva <prog.samdev@gmail.com>
2024-02-03 17:15:00 -0300

    Create data for form 2

--- controllers/users.go
+++ controllers/users.go
@@ -15,11 +15,21 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	var data struct {
 		Email string
 	}
+
 	data.Email = r.FormValue("email")
 	u.Templates.New.Execute(w, data)
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	/*err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprint(w, "Email: ", r.PostForm.Get("email"))
+	fmt.Fprint(w, "Password: ", r.PostForm.Get("password"))*/
+
 	fmt.Fprint(w, "Email: ", r.FormValue("email"))
-	fmt.Fprint(w, "Password: ", r.FormValue("password"))
+	fmt.Fprint(w, "Password: ", r.PostForm.Get("password"))
 }
