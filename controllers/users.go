57877878cb6e435388dd515b63b725d6ebb8616c
samuel silva <prog.samdev@gmail.com>
2024-02-15 14:46:34 -0300

    created user linked to db

--- controllers/users.go
+++ controllers/users.go
@@ -9,7 +9,8 @@ import (
 
 type Users struct {
 	Templates struct {
-		New Template
+		New    Template
+		SignIn Template
 	}
 
 	UserService *models.UserService
@@ -24,7 +25,22 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	password := r.FormValue("password")
 
-	fmt.Fprint(w, "Email: ", r.FormValue("email"))
-	fmt.Fprint(w, "Password: ", r.PostForm.Get("password"))
+	user, err := u.UserService.Create(email, password)
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Error(w, "Something went wrong!", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(w, "User created: %+v", user)
+}
+
+func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+	u.Templates.SignIn.Execute(w, data)
 }
--- main.go
+++ main.go
@@ -34,13 +34,20 @@ func main() {
 	userSer := models.UserService{DB: db}
 
 	usersC := controllers.Users{UserService: &userSer}
+
 	usersC.Templates.New = views.Must(views.ParseFS(
 		templates.FS,
 		"signup.gohtml", "tailwind.gohtml",
 	))
-	r.Get("/signup", usersC.New)
 
+	usersC.Templates.SignIn = views.Must(views.ParseFS(
+		templates.FS,
+		"signin.gohtml", "tailwind.gohtml",
+	))
+
+	r.Get("/signup", usersC.New)
 	r.Post("/signup", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
 
 	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))
 	r.Get("/faq", controllers.FAQ(tpl))
