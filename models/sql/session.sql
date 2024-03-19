d56542ff42facfc23c40287c8aa4acb19d78d466
progsamdev <samuel.msbr@gmail.com>
2024-03-18 22:29:09 -0300

    Improve sql finished

--- models/sql/session.sql
+++ models/sql/session.sql
@@ -1,5 +1,6 @@
 CREATE TABLE sessions (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID UNIQUE,
-    token_hash TEXT UNIQUE NOT NULL
+    token_hash TEXT UNIQUE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
